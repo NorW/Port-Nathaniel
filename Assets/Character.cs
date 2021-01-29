85e4d6fe757e26c3a6e4f38b04fa4add86bc2b4a
KgnDml <47165295+KgnDml@users.noreply.github.com>
2021-01-28 18:28:06 -0800

    Stat files

---
+++ Assets/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+
+    public CharacterStat Health;
+
+    public CharacterStat Fortitude;
+
+    public CharacterStat Spirit;
+
+
+}
+
---
+++ Assets/Stats/ItemStats/Item.cs
@@ -0,0 +1,17 @@
+﻿
+public class Item
+{
+    public void Equip(Character c)
+    {
+        c.Health.AddModifier(new StatModifier(10, StatModType.Flat, this));
+        c.Health.AddModifier(new StatModifier(0.1f, StatModType.PercentMult, this));
+    }
+
+    public void Unequip(Character c)
+    {
+        c.Health.RemoveAllModifiersFromSource(this);
+
+    }
+
+
+}
