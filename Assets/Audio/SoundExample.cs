28f76e3eeada0d953ca05226dbd21152c8923754
KgnDml <47165295+KgnDml@users.noreply.github.com>
2021-01-31 13:18:12 -0800

    singleton Sound Manager
    Also added the SoundExample so you can see how to implement it into any part you need.

---
+++ Assets/Audio/SoundExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundExample : MonoBehaviour
+{
+    public AudioClip BattleMusic;
+
+    void Start()
+    {
+        SoundManager.Instance.PlayMusic(BattleMusic);
+    }
+}
